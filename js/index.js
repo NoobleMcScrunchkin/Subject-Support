2024d605d26c541b8bf92c2c60319dac3c8ac904
itselectroz <cancerouselectroz@gmail.com>
2019-12-18 09:56:05 +0000

    To-Do Button Implemented

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-function complete_period(studentid, period, noreload) {
+function complete_period(studentid, period, todo, noreload) {
     let req = new XMLHttpRequest();
     req.open("POST", "/requests/complete.php", true);
     let data = new FormData();
     data.append('period', period);
     data.append('studentId', studentid);
+    data.append('todo', todo);
 
     req.onload = () => {
         if(noreload == true) {
